e65f3599cbe547322224d47604d3f645fa15aba0
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-15 12:09:44 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,2 +1,19 @@
 require 'pry'
 
+def consolidate_cart(cart)
+  cart.each do |item, details|
+    binding.pry
+  end
+end
+
+def apply_coupons(cart, coupons)
+  # code here
+end
+
+def apply_clearance(cart)
+  # code here
+end
+
+def checkout(cart, coupons)
+  # code here
+end
